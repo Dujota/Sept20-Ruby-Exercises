663562dc96ea147498dd38f576f22629a33883b8
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-20 17:05:26 -0400

    did 3 versions to play around with the different solutions

--- reinforcement.rb
+++ reinforcement.rb
@@ -1,6 +1,38 @@
 puts "What percent grade did you get?"
 percent = gets.chomp.to_i
 
+
+
+def grade(percet)
+  if percet >= 90
+    p "A+"
+  elsif percent >= 85
+    p "A"
+  elsif percent >= 80
+    p "A-"
+  elsif percent >= 75
+    p "B+"
+  elsif percent >= 70
+    p "B"
+  elsif percent >= 65
+    p "B-"
+  elsif percent >= 60
+    p "C"
+  elsif percent >= 55
+    p "C-"
+  elsif percent >= 50
+    p "D"
+  elsif percent <= 49
+    p "F"
+  end
+end
+
+grade(percent)
+
+
+
+#using ranges and syntax for ranges if used with an if loop
+
 def grade(percent)
   if percent >= 80
     "That's an A+"
@@ -37,7 +69,6 @@ def grade2(percent)
     "Thats a C"
   else
     "thats an F bro :("
-
   end
 end
 
